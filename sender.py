2551116c6e5635213f2faffa60a38c268d5e01ad
Edoardo Marchesi Veglia <edoardo.marchesi.veglia@gmail.com>
2024-09-19 22:34:19 +0200

    Update sender.py

--- sender.py
+++ sender.py
@@ -1,5 +1,9 @@
 import sys
 import socket
+import re
+
+DEFAULT_PORT = 5000
+IP_REGEX = r"^\d{1,3}(\.\d{1,3}){3}$"
 
 
 def send_udp_message(ip, port, message):
@@ -8,22 +12,19 @@ def send_udp_message(ip, port, message):
         sock.sendto(message.encode(), (ip, port))
     except socket.gaierror as gai_error:
         print(f"Error: Invalid address or hostname, {gai_error}")
-        sys.exit(1)
+        exit_program(1)
     except socket.error as socket_error:
         print(f"Error: {socket_error}")
-        sys.exit(1)
-    except KeyboardInterrupt:
-        print("\nProgram stopped")
-        sys.exit(0)
+        exit_program(1)
     finally:
         sock.close()
 
 
 def get_port():
     while True:
-        port_input = input("Destination Port (default 7777): ")
+        port_input = input(f"Destination Port (default {DEFAULT_PORT}): ")
         if port_input.strip() == "":
-            return 7777
+            return DEFAULT_PORT
         try:
             port = int(port_input)
             if 1 <= port <= 65535:
@@ -34,16 +35,30 @@ def get_port():
             print("Error: Destination port must be a valid integer.")
 
 
+def validate_ip(ip):
+    if re.match(IP_REGEX, ip):
+        return all(0 <= int(num) <= 255 for num in ip.split("."))
+    return False
+
+
+def exit_program(exit_code=0):
+    print("\nProgram stopped")
+    sys.exit(exit_code)
+
+
 if __name__ == "__main__":
     try:
-        destination_ip = input("Destination IP: ")
-
+        while True:
+            destination_ip = input("Destination IP: ").strip()
+            if validate_ip(destination_ip):
+                break
+            print("Error: Invalid IP address format.")
         destination_port = get_port()
-
         print("Press Ctrl+C to exit")
         while True:
-            message_to_send = input("Message to send: ")
-            send_udp_message(destination_ip, destination_port, message_to_send)
+            message_to_send = input("Message to send: ").strip()
+            if message_to_send:
+                send_udp_message(destination_ip, destination_port, message_to_send)
+
     except KeyboardInterrupt:
-        print("\nProgram stopped")
-        sys.exit(0)
+        exit_program(0)
